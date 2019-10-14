27a1a9c0bb243956b09185f600c971708d965c72
Sukhmanpreet Kaur <53627020+Sukhmanpreet-Kaur@users.noreply.github.com>
2019-10-14 12:27:35 -0500

    Update ViewCustomerPage.aspx.cs

--- ViewCustomerPage.aspx.cs
+++ ViewCustomerPage.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,11 +7,29 @@ using System.Web.UI.WebControls;
 
 namespace CIS470_Senior_Course_Project
 {
-    public partial class ViewCustomerPage : System.Web.UI.Page
+    public partial class ViewCustomer : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            if (!Page.IsPostBack)
+            {
+                // Declare the DataSet
+                dsPersonnel myDataSet = new dsPersonnel();
+
+                string strSearch = "";
+                //Gets Search Name from the request object from the previous form's text box
+                if (Request["txtSearchName"] != null) //Checks if the request string is null
+                    strSearch = Request["txtSearchName"].ToString();
+
+                // Fill the dataset with what is returned from the function
+                myDataSet = clsDataLayer.GetCustomer(Server.MapPath("wsc_v4.mdb"), strSearch);
+                // Set the DataGrid to the DataSource based on the table
+                grdViewCustomer.DataSource = myDataSet.Tables["tblCustomer"];
+
+                // Bind the DataGrid
+                grdViewCustomer.DataBind();
+            }
         }
     }
 }
