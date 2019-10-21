f0a1f35987e3cf708e876d575a5fe870caa30f4e
virgilrico <55720512+virgilrico@users.noreply.github.com>
2019-10-21 14:48:27 -0700

    Update AddOrderPage.aspx.cs

--- AddOrderPage.aspx.cs
+++ AddOrderPage.aspx.cs
@@ -18,25 +18,93 @@ namespace CIS470_Senior_Course_Project
         
         protected void btnAddCustomer_Click(object sender, EventArgs e)
         {
+            
+            bool validatedState = true;
 
-            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|wsc_v4.accdb");
-            conn.Open();
-            OleDbCommand command = conn.CreateCommand();
+            //checks if end date has an input, if not then error message appears
+            if (Request["txtOrderName"].ToString().Trim() == "")
+            {
+                validatedState = false;
+                txtOrderName.BackColor = System.Drawing.Color.Yellow;
+                lblErrorOrderName.Text = "Missing Order Name input";
+                lblErrorOrderName.BackColor = System.Drawing.Color.Red;
+            }
+            
+            //checks if end date has an input, if not then error message appears
+            if (Request["txtCustomerName"].ToString().Trim() == "")
+            {
+                validatedState = false;
+                txtCustomerName.BackColor = System.Drawing.Color.Yellow;
+                lblErrorCustomerName.Text = "Missing Customer Name input";
+                lblErrorCustomerName.BackColor = System.Drawing.Color.Red;
+            }
+            
+            //checks if end date has an input, if not then error message appears
+            if (Request["txtOrderDate"].ToString().Trim() == "")
+            {
+                validatedState = false;
+                txtOrderDate.BackColor = System.Drawing.Color.Yellow;
+                lblErrorOrderDate.Text = "Missing Order Date input";
+                lblErrorOrderDate.BackColor = System.Drawing.Color.Red;
+            }
+            
+            //checks if end date has an input, if not then error message appears
+            if (Request["txtOrderText"].ToString().Trim() == "")
+            {
+                validatedState = false;
+                txtOrderText.BackColor = System.Drawing.Color.Yellow;
+                lblErrorOrderText.Text = "Missing Order Text input";
+                lblErrorOrderText.BackColor = System.Drawing.Color.Red;
+            }
+            
+            //checks if end date has an input, if not then error message appears
+            if (Request["txtOrderPrice"].ToString().Trim() == "")
+            {
+                validatedState = false;
+                txtOrderPrice.BackColor = System.Drawing.Color.Yellow;
+                lblErrorOrderPrice.Text = "Missing Order Price input";
+                lblErrorOrderPrice.BackColor = System.Drawing.Color.Red;
+            }
+            
+            //checks if end date has an input, if not then error message appears
+            if (Request["txtOrderStatus"].ToString().Trim() == "")
+            {
+                validatedState = false;
+                txtOrderStatus.BackColor = System.Drawing.Color.Yellow;
+                lblErrorOrderStatus.Text = "Missing Order Status input";
+                lblErrorOrderStatus.BackColor = System.Drawing.Color.Red;
+            }
+            
+            if (validatedState == true)
+            {
+                try
+                {
+                    OleDbConnection conn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|wsc_v4.mdb");
+                    conn.Open();
+                    OleDbCommand command = conn.CreateCommand();
 
-            string strSQL;
-            string orderText = txtOrderName.Text;
+                    string strSQL;
+                    string orderName = txtOrderName.Text;
+                    string customerName = txtCustomerName.Text;
+                    string inventoryName = ddlInventoryName.SelectedValue.ToString();
+                    string orderDate = txtOrderDate.Text;
+                    string orderText = txtOrderText.Text;
+                    string orderPrice = txtOrderPrice.Text;
+                    string orderStatus = txtOrderStatus.Text;
 
-            strSQL = "Insert into tblOrder " + "( orderText) VALUES ( '" + orderText + "')";
+                    strSQL = "Insert into tblOrder " + "( orderName, customerName, inventoryName, orderDate, orderText, orderPrice, OrderStatus) VALUES ( '" + orderName + "','" + customerName + "','" + inventoryName + "','" + orderDate + "','" + orderText + "','" + orderPrice + "','" + orderStatus + "')";
 
-            command.CommandType = CommandType.Text;
-            command.CommandText = strSQL;
-            command.ExecuteNonQuery();
-            conn.Close();
-
-            Response.Redirect("AddOrder.aspx");
-
-
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = strSQL;
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                    
+                    lblError.Text = "Order was saved.";
+                    Response.Redirect("AddOrderPage.aspx");
         }
-        
+            catch(Exception ex)
+                {
+                    lblError.Text = "The order was not saved.";
+                }
     }
 }
