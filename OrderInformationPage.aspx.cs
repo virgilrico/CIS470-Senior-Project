2a17fb14ad75182e64f63f383cc430af27cca08c
Sukhmanpreet Kaur <53627020+Sukhmanpreet-Kaur@users.noreply.github.com>
2019-10-24 12:54:11 -0500

    Update OrderInformationPage.aspx.cs

--- OrderInformationPage.aspx.cs
+++ OrderInformationPage.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
 
 namespace CIS470_Senior_Course_Project
 {
@@ -11,7 +13,45 @@ namespace CIS470_Senior_Course_Project
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Checks user roles from the session object
+            if (Session["securityLevel"] != "S")
+            {
+                linkCreateNewOrder.Visible = false;
+                imgCreateNewOrder.Visible = false;
+             
+            }
+            if (Session ["securityLevel"] != "O") //If the user is not Operations Manager, EditValidate order link and image is not visible
+            {
+                linkEditValidateOrder.Visible = false;
+                imgEditValidateOrder.Visible = false;
 
+
+            }
+            if (Session["securityLevel"] == "O") //If the user is Operations manager, edit order link and image is not visible
+            {
+                linkEditOrder.Visible = false;
+                imgEditOrder.Visible = false;
+            }
+            if (Session["securityLevel"] == "C") //If the user is Stcok Room Clerk, edit order link and image is not visible
+            {
+                linkEditOrder.Visible = false;
+                imgEditOrder.Visible = false;
+            }
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            bool validatedState = true;
+
+            //checks if Order Name has an input, if not then error message appears
+            if (Request["txtSearchOrder"].ToString().Trim() == "")
+            {
+                validatedState = false;
+                txtSearchOrder.BackColor = System.Drawing.Color.Yellow;//change the back color of the text box 
+                lblErrorOrder.Text = "Missing Order Name input";    //set error message
+                lblErrorOrder.BackColor = System.Drawing.Color.Red; //change the back color of the error label
+            }
         }
     }
 }
