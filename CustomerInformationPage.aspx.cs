91f7d6c49cfd28d0c97c5ed2f1d764b8b0dc4cd1
Sukhmanpreet Kaur <53627020+Sukhmanpreet-Kaur@users.noreply.github.com>
2019-10-24 12:39:39 -0500

    Update CustomerInformationPage.aspx.cs

--- CustomerInformationPage.aspx.cs
+++ CustomerInformationPage.aspx.cs
@@ -11,22 +11,27 @@ namespace CIS470_Senior_Course_Project
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
-        }
-
-        protected void LinkButton3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void LinkButton1_Click(object sender, EventArgs e)
-        {
-
+            if (Session["securityLevel"] != "S") //If user is not Salesperson, add customer link & image are not visible
+            {
+                linkbtnAddCustomer.Visible = false;
+                imgBtnAddCustomer.Visible = false;
+            }
         }
 
         protected void btnSearch_Click(object sender, EventArgs e)
         {
+            //Sets bool to true
+            bool validatedState = true;
 
+            //checks if search textbox has an input, if not then error message appears
+            if (Request["txtSearchName"].ToString().Trim() == "")
+            {
+                validatedState = false;                                       //sets bool to false
+                txtSearchName.BackColor = System.Drawing.Color.Yellow;        //colors text box yellow
+                lblErrorSearchLastName.Text = "Missing item Name input";      //set error label text
+
+                lblErrorSearchLastName.BackColor = System.Drawing.Color.Red;  //colors error label red
+            }
         }
     }
 }
