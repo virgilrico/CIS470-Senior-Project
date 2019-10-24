e20697a395cf0531fb153aa87149af111e186d6f
Sukhmanpreet Kaur <53627020+Sukhmanpreet-Kaur@users.noreply.github.com>
2019-10-24 12:59:10 -0500

    Update UserAccessInformationPage.aspx.cs

--- UserAccessInformationPage.aspx.cs
+++ UserAccessInformationPage.aspx.cs
@@ -7,46 +7,75 @@ using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.OleDb;
 
-namespace CIS470_Senior_Project
+namespace CIS470_Senior_Course_Project
 {
     public partial class UserAccessInformationPage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
         }
-
-        protected void btnAddUser_Click(object sender, EventArgs e)
+        protected void Button1_Click(object sender, EventArgs e)
         {
-            
-            try {
-                OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|wsc_v4.accdb");
-                conn.Open();
-                OleDbCommand command = conn.CreateCommand();
+            //sets bool as true
+            bool validatedState = true;
 
-                string strSQL;
-                string username = txtUsername.Text;
-                string password = txtPassword.Text;
-                string securityLevel = ddlSecurityLevel.SelectedValue.ToString();
-
-                strSQL = "Insert into tblUserAccess " + "( userName, userPassword, securityLevel) VALUES ('" + username + "','" + password + "','" + securityLevel + "')";
-
-                command.CommandType = CommandType.Text;
-                command.CommandText = strSQL;
-                command.ExecuteNonQuery();
-                conn.Close();
-
-                
-                lblError.Text = "User was saved.";
-            }
-            catch (Exception ex)
+            //checks if Username text box has an input, if not then error message appears
+            if (Request["txtUserName"].ToString().Trim() == "")
             {
-                
-                lblError.Text = "User was not saved.";
+                validatedState = false;                                 //sets bool to false
+                txtUserName.BackColor = System.Drawing.Color.Yellow;    //colors text box yellow
+                lblErrorUserName.Text = "Missing Username input";       //sets error label text
+                lblErrorUserName.BackColor = System.Drawing.Color.Red;  //colors error label red
             }
 
-            Response.Redirect("UserAccessInformationPage.aspx");
+            //checks if password text box has an input, if not then error message appears
+            if (Request["txtPassword"].ToString().Trim() == "")
+            {
+                validatedState = false;                                 //sets bool to false
+                txtPassword.BackColor = System.Drawing.Color.Yellow;    //colors text box yellow
+                lblErrorPassword.Text = "Missing Password input";       //sets error label text
+                lblErrorPassword.BackColor = System.Drawing.Color.Red;  //colors error label red
 
+            }
+
+            //Checks if bool is true
+            if (validatedState == true)
+            {
+                try
+                {  
+                    //Create a new connection object to the Access database
+                    OleDbConnection conn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|wsc_v4.mdb");
+
+                    //Open the database connection
+                    conn.Open();
+                    OleDbCommand command = conn.CreateCommand();
+
+                    string strSQL;
+                    string username = txtUserName.Text;
+                    string password = txtPassword.Text;
+                    string securityLevel = cboRole.SelectedValue.ToString();
+
+                    // Inserts into user access database table
+                    strSQL = "Insert into tblUserAccess " + "( userName, userPassword, securityLevel) VALUES ('" + username + "','" + password + "','" + securityLevel + "')";
+
+                    // Fills user input into database table and closes connection
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = strSQL;
+                    command.ExecuteNonQuery();
+                    conn.Close();
+
+                    //Notifies user input was saved and reloads page
+                    lblNewUserStatus.Text = "User was saved.";
+                    Response.Redirect("UserAccessInformationPage.aspx");
+                }
+
+                catch (Exception ex)
+                {
+                    // Notifies user that data was not saved
+                    lblNewUserStatus.Text = "User was not saved.";
+                }
+            }
         }
     }
 }
