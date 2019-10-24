ea6a2d42efa71495fca45d455eddca00c57d53d9
Sukhmanpreet Kaur <53627020+Sukhmanpreet-Kaur@users.noreply.github.com>
2019-10-24 12:48:00 -0500

    Update InventoryInformationPage.aspx.cs

--- InventoryInformationPage.aspx.cs
+++ InventoryInformationPage.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
 
 namespace CIS470_Senior_Course_Project
 {
@@ -11,7 +13,33 @@ namespace CIS470_Senior_Course_Project
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Checks user roles from the session object
+            if (Session["securityLevel"] != "P" && Session["securityLevel"] != "C")  //If user is not Specialist or Clerk. Search panel, and edit inventory link & image are not visible
+            {
+                SearchPanelPrintingAndClerk0.Visible = false;
+                imgEditInventory.Visible = false;
+                linkEditInventory.Visible = false;
 
+            }
+   
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            //sets bool as false
+            bool validatedState = true;
+
+            //Checks if search text box has an input, if not then error message appears
+            if (Request["txtSearchItem"].ToString().Trim() == "")
+            {
+                validatedState = false;                                  //sets bool as false
+                txtSearchItem.BackColor = System.Drawing.Color.Yellow;   //colors text box yellow
+                lblErrorInventory.Text = "Missing item Name input";      //set error label text
+                lblErrorInventory.BackColor = System.Drawing.Color.Red;  //colors error label red
+            }
         }
     }
 }
+
+
+       
